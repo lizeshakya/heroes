48b205af9eabd366de6a3fc06f9c352c63fc88a6
lizeshakya <lizeshakya@gmail.com>
2019-01-18 12:13:18 +0545

    Added the message alert

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './heroes/hero';
 import { HEROES } from './heroes/mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service'; 
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,10 @@ import { Observable, of } from 'rxjs';
 
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
